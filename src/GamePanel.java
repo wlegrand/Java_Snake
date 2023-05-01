30c45e823e0f752bbd9a2a796e3a9606a9afa0a1
wlegrand <legrand.william.ch@gmail.com>
2023-05-01 20:15:36 +0200

    Setting up project

---
+++ src/GamePanel.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener {
+    static  final int Screen_WIDTH = 600;
+    static  final int Screen_HEIGHT = 600;
+
+    GamePanel(){
+
+    }
+
+    public void startGame(){
+
+    }
+    public void paintComponent(Graphics g){
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
+}
